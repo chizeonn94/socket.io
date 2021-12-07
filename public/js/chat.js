cd1cd139c1e769e2f98fc02445b7a00e0ee0449b
Jisun Kim <jisun@Jisuns-MacBook-Air.local>
2021-12-07 22:34:49 +0900

    added sendLocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,33 @@
 const socket = io();
 
-socket.on("countUpdated", (count) => {
-  console.log("the count has been updated !!", count);
+// socket.on("countUpdated", (count) => {
+//   console.log("the count has been updated !!", count);
+// });
+
+// document.querySelector("#increment").addEventListener("click", () => {
+//   socket.emit("increment");
+// });
+
+socket.on("message", (msg) => console.log(msg));
+socket.on("showMsg", (msg) => console.log("message >> ", msg));
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.messageInput.value;
+  //   console.log("message>>>", message);
+  socket.emit("sendMsg", message);
 });
 
-document.querySelector("#increment").addEventListener("click", () => {
-  socket.emit("increment");
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    alert("Geolocation is not supported in this browser");
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
 });
--- src/index.js
+++ src/index.js
@@ -12,15 +12,32 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
+//let count = 0;
+const welcomeMsg = "welcome!";
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
-  socket.emit("countUpdated", count);
-  socket.on("increment", () => {
-    count++;
-    //socket.emit("countUpdated", count);
-    io.emit("countUpdated", count);
+  //   socket.emit("countUpdated", count);
+  //   socket.on("increment", () => {
+  //     count++;
+  //     //socket.emit("countUpdated", count);
+  //     io.emit("countUpdated", count);
+  //   });
+  socket.broadcast.emit("message", "A new user joined!");
+  socket.emit("message", welcomeMsg);
+  socket.on("sendMsg", (msg) => {
+    io.emit("showMsg", msg);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user left!");
+  });
+
+  socket.on("sendLocation", (position) => {
+    console.log(position);
+    io.emit(
+      "message",
+      `Location : ${position.latitude + " " + position.longitude}`
+    );
   });
 });
 
