d0515ea4b9ac781e53ce1bd8ef8ed9821a0da9b7
Jisun Kim <jisun@Jisuns-MacBook-Air.local>
2021-12-07 20:07:17 +0900

    socket.emit -> io.emit

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ io.on("connection", (socket) => {
   socket.emit("countUpdated", count);
   socket.on("increment", () => {
     count++;
-    socket.emit("countUpdated", count);
+    //socket.emit("countUpdated", count);
+    io.emit("countUpdated", count);
   });
 });
 
